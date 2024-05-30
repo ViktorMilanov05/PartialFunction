2ee2083141bf2e57d0bb32d299277cab99ac90a8
ViktorMilanov05 <viktormilanov12@gmail.com>
2024-05-30 23:03:47 +0300

    Partial Function task is done

--- HomeWorkUP2/HomeWorkUP2/FuncWithExcludedPoints.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Func.h"
-
-class FuncWithExcludedPoints : public Func {
-public:
-	FuncWithExcludedPoints(int32_t* results, int32_t count);
-	bool isExcluded(int32_t point) const override;
-	int32_t operator()(int32_t point) const override;
-};
